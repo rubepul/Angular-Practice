7b3c6ab5f03b9b0f2cc3d89ae1be2818d913cd69
Ruben Pulido <Ruben.Pulido@co.kings.ca.us>
2024-02-13 11:57:00 -0800

    Every entry time entry fills the entire row

--- mock-proj/src/app/table/table.component.ts
+++ mock-proj/src/app/table/table.component.ts
@@ -1,22 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-table',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './table.component.html',
   styleUrl: './table.component.css'
 })
 export class TableComponent {
+
+  activity = [
+    {
+      id: '',
+      hours: '',
+      email: '',
+    },
+    {
+      id: '',
+      hours: '',
+      email: '',
+    },
+    {
+      id: '',
+      hours: '',
+      email: '',
+    },
+  ];
+  
   //Days in week and calendar period
   datesInWeek = 7;
   datesInCalendar = 14
+  refreshTime = new Date().getDate() - 12;
 
   // Arrays for the week, calendar, and dates (month/day)
-  weekly = ['Monday', 'Tuesday', 'Wendesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+  weekly = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
-  calendar = ['Monday', 'Tuesday', 'Wendesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 
-  'Monday', 'Tuesday', 'Wendesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+  calendar = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 
+  'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
   dates = [];
 
@@ -39,12 +61,36 @@ export class TableComponent {
   }
 
   // Helper function that creates dates (month/day) format
-  createDates(amount) {
+  createDates(amount: any) {
     this.dates = [];
+    
+    if (this.refreshTime < 0) {
+      this.refreshTime = new Date().getDate();
+    }
+
     for (let index = 0; index < amount; index++) {
-      let day = new Date().getDate() + index;
+      let day = new Date().getDate() + index - this.refreshTime;
       let month = new Date().getMonth();
       this.dates.push(month + "/" + day);
     }
   }
+
+  addRow(position: any) {
+    const obj = {
+      id: '',
+      hours: '',
+      email: '',
+    };
+    this.activity.splice(position, 0, obj)
+    // this.activity.push(obj);
+  }
+
+  deleteRow(row: any) {
+    const deleteButton = confirm(' Do you want to delete ?');
+    if (deleteButton == true) {
+      this.activity.splice(row, 1);
+    }
+  }
+
+  
 }
