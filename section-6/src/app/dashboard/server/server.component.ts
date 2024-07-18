5119057c749bee2a0a11e3d9d923b4c8411d0bf1
Ruben Pulido <Ruben.Pulido@co.kings.ca.us>
2024-07-18 08:13:17 -0700

    Completed HostElements content, Binding CSS classes dynamically, and Inline style binding

--- section-6/src/app/dashboard/server/server.component.ts
+++ section-6/src/app/dashboard/server/server.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server',
@@ -7,7 +7,24 @@ import { Component } from '@angular/core';
   templateUrl: './server.component.html',
   styleUrl: './server.component.css'
 })
-export class ServerComponent {
-  currentStatus = 'online';
+export class ServerComponent implements OnInit{
+  // Example of Literal Types
+  currentStatus: 'online' | 'offline' | 'unknown' = 'online';
 
+  constructor(){
+  }
+
+  ngOnInit() {
+    setInterval(() => {
+      const rnd = Math.random();
+      console.log(rnd)
+      if (rnd < 0.5){
+        this.currentStatus = 'online';
+      } else if (rnd < 0.9) {
+        this.currentStatus = 'offline';
+      } else {
+        this.currentStatus = 'unknown';
+      }
+    }, 5000);
+  }
 }
--- section-6/src/app/shared/control/control.component.ts
+++ section-6/src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ViewEncapsulation } from '@angular/core';
+import { Component, ElementRef, inject, Input, ViewEncapsulation } from '@angular/core';
 
 // With ViewEncapsulation.None the css styles in the styleUrl will become global styles
 
@@ -8,9 +8,22 @@ import { Component, Input, ViewEncapsulation } from '@angular/core';
   imports: [],
   templateUrl: './control.component.html',
   styleUrl: './control.component.css',
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
+  // Will add the class property whenever app-control wherever it's being used (used instead of HostBinding)
+  // Can also listen to events (used instead of HostListener)
+  host: {
+    class: 'control',
+    '(click)': 'onClick()'
+  }
 })
 export class ControlComponent {
   @Input({required: true}) label!: string;
+
+  // Accessing Host Elements Programmatically
+  private el = inject(ElementRef)
+
+  onClick() {
+    console.log(this.el)
+  }
 
 }
